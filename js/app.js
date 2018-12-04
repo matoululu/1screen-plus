56dc98b04ace2434dee2accbb0beda17d20e87c1
Matthew Marineau <matoululu@users.noreply.github.com>
2018-12-04 18:16:18 -0500

    MINIFY!

--- js/app.js
+++ js/app.js
@@ -472,9 +472,6 @@ function isPremium() {
   // });
 }
 
-//
-//
-
 var tipsArray = [
   '<h3>Did you know?</h3><p>Your changes save automatically every 5 minutes.</p>',
   '<h3>Did you know?</h3><p>You can store useful snippets by clicking the "notes" button.</p>',
