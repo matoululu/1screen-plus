8b10e5b2432cf1afb52001497985bfa273316edd
Matthew Marineau <matoululu@users.noreply.github.com>
2019-01-11 18:57:32 -0500

    Sneak update

--- js/globals.js
+++ js/globals.js
@@ -1,9 +1,9 @@
-var appPage = "/campaigns";
-var termsPage = "/terms#tos";
-var privacyPage = '/terms#privacy'
-var homePage = "/";
-var loginPage = "/login";
-var editorPage = "/editor";
+const appPage = "/campaigns";
+const termsPage = "/terms#tos";
+const privacyPage = '/terms#privacy'
+const homePage = "/";
+const loginPage = "/login";
+const editorPage = "/editor";
 var userName, userEmail, userId;
 var userPremium = false;
 var campaignInfo = new Object();
@@ -15,4 +15,5 @@ var commandList = document.getElementById('commands');
 var tipsArray = [
   '<h3>Did you know?</h3><p>Your changes save automatically every 5 minutes.</p>',
   '<h3>Did you know?</h3><p>You can store useful snippets by clicking the "notes" button.</p>',
+  '<h3>Like 1Screen?</h3><p>If so, please consider <a href="https://www.buymeacoffee.com/matouio" target="_blank">showing your support</a></p>'
 ]
